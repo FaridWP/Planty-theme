fd82304257ab87c556874a5bafb1e13229d4d667
Farid <farid.benmebarek@gmail.com>
2023-12-12 20:52:05 +0100

    Création page Accueil

--- functions.php
+++ functions.php
@@ -18,3 +18,14 @@ function planty_config()
     ));
 }
 add_action('after_setup_theme', 'planty_config', 0);
+
+
+function remove_wp_block_library_css()
+{
+    wp_dequeue_style('wp-block-library'); // Remove WordPress core CSS
+    //wp_dequeue_style('wp-block-library-theme'); // Remove WordPress theme core CSS
+    //wp_dequeue_style('classic-theme-styles'); // Remove global styles inline CSS
+    //wp_dequeue_style('wc-block-style'); // Remove WooCommerce block CSS
+    //wp_dequeue_style('global-styles'); // Remove theme.json css
+}
+add_action('wp_enqueue_scripts', 'remove_wp_block_library_css', 100);
